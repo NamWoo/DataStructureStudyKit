f5f8b9bb5837314b452dd7fade6566e44eddb62a
bkh_desktop <bkh751@gmail.com>
2019-06-03 23:38:45 +0900

    구현해야할 프로토타입들 몇가지 추가, 결과 출력 함수, 자료구조 함수, 점수매기기함수, 모듈 싱행함수등

--- list/TestFiles/Tester.h
+++ list/TestFiles/Tester.h
@@ -1,11 +1,26 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef int (*ArgsFuncPtr)(int argc, ...);
+
 void InitTester(int testArgCounts,...);
 void PrintResult(int totalTestCounts);
 void TestAndScore(int functionNum,int argc,...);
 
-typedef int (*ArgsFuncPtr)(int argc, ...);
+//TODO
+// 구현해야함 
+void PrintResult();
+
+//ERROR_CODE 발생, SUCCESS_CODE 발생, 해당 함수에 해당되는 인자수 입력, 인자입력
+int TestDataStructureFunction(ArgsFuncPtr functionForTest , int argc,...); 
+
+//ERROR_CODE 에 따른 성적 
+void TestScoring(); 
+
+//각 테스트 모듈을 실행한다 
+//(initialize module, insertion module, delete module, search module, destroy module)
+void  TestModuleStarter(ArgsFuncPtr);
+
 
 enum TestSize
 {
