87824fd6185cb77fd3950cb0b71c3e9661035d20
백경호 <bkh751@gmail.com>
2019-06-03 18:51:50 +0900

    불필요한것들 제거

--- list.c
+++ list.c
@@ -8,9 +8,7 @@
 //  리스트 초기화하기 (초기화할 리스트 받기, 동적할당 해제를 해주는 함수등록하기)
 int InitList(List* list)
 {
-    printf("리스트 초기화중...");
-    list->Head = NULL;
-    list->Tail = NULL;
+
 }
 
 
@@ -18,10 +16,7 @@ int InitList(List* list)
 // 노드를 리스트의 맨 마지막에 넣는다 실패시 -1, 성공시 0반환
 int AddNode(int data, List* list)
 {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->Data = data;
-    list->Tail->Next = newNode;
-    list->Tail = newNode;
+
 }
 
 // 리스트의 사이즈를 반환
@@ -49,15 +44,5 @@ void TestList()
 //  리스트 제거하기 제거할게 없으면 -1 반환 성공시 0
 int DestroyList(List* list)
 {
-    printf("노드 제거중...\n");
-    Node* temp = list->Head;
-    
-    while (temp)  
-    {
-        free(temp);
-        temp = temp->Next;
-    }
 
-    free(list);
-    return 0;
 }
