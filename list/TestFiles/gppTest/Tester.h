a48b7969025b26f5825dad00ad0128d636065e5f
백경호 <bkh751@gmail.com>
2019-06-04 17:09:35 +0900

    삽질중

--- list/TestFiles/gppTest/Tester.h
+++ list/TestFiles/gppTest/Tester.h
@@ -1,32 +1,59 @@
-typedef int (*VaFunc)(...);
+#include <iostream>
+typedef int (*VaFunc_t)(...);
 
+enum ERROR_CODE
+{
+    SUCCESS = 0,
+    FAIL = -1
+};
 
-
-template <typename T, typename ...Args>
+template <typename T, typename... Args>
 class Tester
 {
 private:
     int mCurrentTestNum;
-public:
-    Tester();
-    ~Tester();
+    VaFunc_t mCurrentTest;
 
-    static void TestVariadic(Args... arg, T evaluateValue);
+public:
+    Tester() = default;
+    Tester(T func, int testCases);
+    ~Tester();
+    void TestRegisteredFunc(Args... args);
+    static void TestFunc(T func, Args... args);
 };
 
+template <typename T, typename... Args>
+Tester<T, Args...>::Tester(T func, int testCases)
+{
+    mCurrentTest = (VaFunc_t)func;
+    mCurrentTestNum = 0;
+}
 
-template <typename ...Args>
-Tester<Args...>::Tester()
+template <typename T, typename... Args>
+Tester<T, Args...>::~Tester()
 {
 }
 
-template<typename ...Args>
-Tester<Args...>::~Tester()
+template <typename T, typename... Args>
+void Tester<T, Args...>::TestRegisteredFunc(Args... args)
 {
+    mCurrentTest(args...);
 }
 
-template<typename ...Args>
-void Tester<Args...>::TestVariadic(Args...)
+template <typename T, typename... Args>
+void Tester<T, Args...>::TestFunc(T func, Args... args)
 {
-    VaFunc v(Args...);
+
+    switch (func(args...))
+    {
+    case SUCCESS:
+        std::cout << "case Success";
+        break;
+    case FAIL:
+        std::cout << "case end";
+        break;
+    default:
+        std::cout << " i don't know ";
+        break;
+    }
 }
--- list/TestFiles/gppTest/test.cpp
+++ list/TestFiles/gppTest/test.cpp
@@ -1,8 +1,20 @@
 #include "Tester.h"
+#include <iostream>
+int myStrlen(const char* str,int num)
+{
+    char* p = (char*)str;
+    int cnt=0;
+    while (*p++)
+    {
+        std::cout<<cnt++;
+    }
+    std::cout<<std::endl;
+    return 0;
+}
 
 
 int main()
 {
-    Tester t;
+    Tester<VaFunc_t,const char*, int>::TestFunc((VaFunc_t)myStrlen,"asdf",30);
 }
 
