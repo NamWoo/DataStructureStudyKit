3a4ed9ce9ed69ce3f03867ade05e9106e61f0c92
bkh_desktop <bkh751@gmail.com>
2019-06-05 00:55:34 +0900

    리눅스버젼(make 사용) 과 윈도우즈 버젼으로 나눔...( MSVC)

---
+++ Windows/DataStructure/list/TEST_MACRO.h
@@ -0,0 +1,10 @@
+#define EMPTY_SPACE printf("\n\n\n\n\n");
+
+#define TEST_START(description) printf("# %s Start Testing \n",#description);
+
+#define TEST_FAIL(description) printf("# %s Failed...\n",#description);
+
+#define STAGE_CLEAR(stage) printf("=======%s Stage Clear!========\n\n\n",#stage);
+
+#define PRINT_SCORE(score) printf("=====Current Score is %s =======\n",#score);
+
---
+++ Windows/DataStructure/list/TestInitialization.cpp
@@ -0,0 +1,15 @@
+#include "TestInitialization.h"
+#include "Tester.h"
+#include "list.h"
+
+
+void TestInitialization()
+{
+	List* myList = (List*)malloc(sizeof(List));
+
+	Tester<VaFunc_t,List*> initTester((VaFunc_t)InitList,1,"리스트 초기화");
+	initTester.TestRegisteredFunc(0,myList);
+
+	free(myList);
+}
+
